d25687228569993e4e20eb87d4dcaafd175c0207
Kingsley Effiong <122705122+kingsfavy@users.noreply.github.com>
2025-04-28 18:45:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,24 @@ function openModal() {
     document.getElementById("loginModal").style.display = "none";
     document.getElementById("mainContent").classList.remove("blur");
   }
-  
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
 
 
 const showCalendarBtn = document.getElementById('showCalendarBtn');
@@ -159,34 +176,5 @@ document.getElementById('showNames').style.display = "block";
             
         } 
 
-function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
 
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
-
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
-
-window.addEventListener("scroll", reveal);
-
-
-// Smooth scrolling
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
 
